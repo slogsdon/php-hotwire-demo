e962cd7d04af3a7f2531b0e40acac107af646bb7
Shane Logsdon <shane@shanelogsdon.com>
2020-12-22 17:35:16 -0500

    add lazy load demo

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <!DOCTYPE html>
 <script type="module" defer src="https://cdn.skypack.dev/@hotwired/turbo"></script>
+
+<turbo-frame id="lazy-loaded-content" src="/lazy-loaded-content.php"></turbo-frame>
---
+++ lazy-loaded-content.php
@@ -0,0 +1,5 @@
+<turbo-frame id="lazy-loaded-content">
+  <h1>Page Title</h1>
+  
+  <p>This is loaded lazily.</p>
+</turbo-frame>
